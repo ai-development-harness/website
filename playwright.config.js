83872f661ce34c446288db97605fc2712570f2fd
yarkovaleksei <yarkov_aleksei@mail.ru>
2026-09-15 20:21:01 +0300

    Исправил тестовый конфиг и CI

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: './tests',
@@ -13,17 +13,33 @@ export default defineConfig({
     baseURL: 'http://127.0.0.1:4173',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure'
+    video: 'retain-on-failure',
   },
   projects: [
-    { name: 'desktop-chromium', use: { ...devices['Desktop Chrome'], viewport: { width: 1440, height: 1000 } } },
-    { name: 'tablet-chromium', use: { ...devices['iPad Pro 11'] } },
-    { name: 'mobile-chromium', use: { ...devices['Pixel 7'] } }
+    {
+      name: 'desktop-chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1440, height: 1000 },
+      },
+    },
+    {
+      name: 'tablet-webkit',
+      use: {
+        ...devices['iPad Pro 11'],
+      },
+    },
+    {
+      name: 'mobile-chromium',
+      use: {
+        ...devices['Pixel 7'],
+      },
+    },
   ],
   webServer: {
     command: 'npm run preview',
     port: 4173,
     reuseExistingServer: !process.env.CI,
-    timeout: 120_000
-  }
-});
+    timeout: 120_000,
+  },
+})
