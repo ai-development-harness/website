b6b13186690eae3151852dbf4e2ab8b1d41c7d21
Yarkov Aleksey <yarkovaleksei@users.noreply.github.com>
2026-09-16 12:31:51 +0300

    test: прогонять полный набор во всех браузерных профилях

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,5 @@
 import { defineConfig, devices } from '@playwright/test'
 
-const responsiveTestMatch = [
-  '**/home/**/*.spec.js',
-  '**/responsive/**/*.spec.js',
-  '**/navigation/header.spec.js',
-  '**/accessibility/keyboard.spec.js',
-]
-
 export default defineConfig({
   testDir: './tests',
   timeout: 30_000,
@@ -32,14 +25,12 @@ export default defineConfig({
     },
     {
       name: 'tablet-webkit',
-      testMatch: responsiveTestMatch,
       use: {
         ...devices['iPad Pro 11'],
       },
     },
     {
       name: 'mobile-chromium',
-      testMatch: responsiveTestMatch,
       use: {
         ...devices['Pixel 7'],
       },
